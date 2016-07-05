cba7f2b6f1811693434d105aa377a4d83baa5505
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2016-07-05 21:27:40 +0530

    Introducing ASP.NET Core.

---
+++ TextAnalyticsBot.WebApp/Model/AppSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TextAnalyticsBot.WebApp.Model
+{
+    public class AppSetting
+    {
+        public string BaseUrl { get; set; }
+        public string AccountKey { get; set; }
+        public int NumLanguages { get; set; }
+    }
+}
